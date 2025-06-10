3003c62fdb594d4fb493380b40ce7550062860ea
n0099 <n@n0099.net>
2025-06-10 12:37:02 +0000

    Move tests that using `OVER()` function from `Query\Mysql\LagTest` and `Query\Mysql\LeadTest` to `Query\Mysql\OverTest`

---
+++ tests/Query/Mysql/OverTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace DoctrineExtensions\Tests\Query\Mysql;
+
+use DoctrineExtensions\Tests\Query\MysqlTestCase;
+
+class OverTest extends MysqlTestCase
+{
+    public function testLead(): void
+    {
+        $this->assertDqlProducesSql(
+            'SELECT OVER(LEAD(COUNT(b.id)), ORDER BY b.id) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT LEAD(COUNT(b0_.id)) OVER (ORDER BY b0_.id ASC) AS sclr_0 FROM Blank b0_'
+        );
+
+        $this->assertDqlProducesSql(
+            'SELECT OVER(LEAD(COUNT(b.id))) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT LEAD(COUNT(b0_.id)) OVER () AS sclr_0 FROM Blank b0_'
+        );
+
+        $this->assertDqlProducesSql(
+            'SELECT OVER(COUNT(b.id) - LEAD(COUNT(b.id)), ORDER BY b.id) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT COUNT(b0_.id) - LEAD(COUNT(b0_.id)) OVER (ORDER BY b0_.id ASC) AS sclr_0 FROM Blank b0_'
+        );
+    }
+
+    public function testLag(): void
+    {
+        $this->assertDqlProducesSql(
+            'SELECT OVER(LAG(COUNT(b.id)), ORDER BY b.id) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT LAG(COUNT(b0_.id)) OVER (ORDER BY b0_.id ASC) AS sclr_0 FROM Blank b0_'
+        );
+
+        $this->assertDqlProducesSql(
+            'SELECT OVER(LAG(COUNT(b.id))) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT LAG(COUNT(b0_.id)) OVER () AS sclr_0 FROM Blank b0_'
+        );
+
+        $this->assertDqlProducesSql(
+            'SELECT OVER(COUNT(b.id) - LAG(COUNT(b.id)), ORDER BY b.id) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT COUNT(b0_.id) - LAG(COUNT(b0_.id)) OVER (ORDER BY b0_.id ASC) AS sclr_0 FROM Blank b0_'
+        );
+    }
+}
